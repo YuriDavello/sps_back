7b6aee3e71fb1b33ee3fd6a252a179821c33ab3a
yuridavello <yuridavello@hotmail.com>
2022-01-30 23:00:25 -0300

    authenticate method

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = require('../../config/secret');
 const router = express.Router();
 
 const User = require('../models/users');
+
+const generateToken = (params = {}) => {
+    return jwt.sign(params, secret.key, {
+        expiresIn: 86400
+    });
+};
 
 router.post('/register', async (req, res) => {
 
     const {email} = req.body;
 
     try {
-        // verify if email already exists   
         if(await User.findOne( {email} ))
             return res.status(400).send({
                 status: 400,
@@ -17,7 +25,7 @@ router.post('/register', async (req, res) => {
 
         const user = await User.create(req.body);
 
-        // user.password = undefined;
+        user.password = undefined;
 
         return res.status(301).send({ 
             user: user,
@@ -34,15 +42,45 @@ router.post('/register', async (req, res) => {
     }
 });
 
-// router.post('/authenticate', async (req, res) => {
+router.post('/authenticate', async (req, res) => {
+    const {email, password} = req.body;
 
-//     try {
-//         const test = await Test.create(req.body);
-//         return res.status(301).send({ test: test });
-//     } catch() {
+    try {
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).send({
+                status: 400,
+                message: 'user not found'
+            });
+        }
 
-//     }
-// });
+        const compare = await bcrypt.compare(password, user.password);
+        if (!compare) {
+            return res.status(400).send({
+                status: 400,
+                message: 'invalid password'
+            });
+        }
+
+        user.password = undefined;
+
+        // gerar token jwt
+        const token = generateToken({  id: user.id });
+        return res.status(200).send({ 
+            user: user,
+            token: token,
+            status: 200,
+            message: 'user authenticated'
+        });
+
+    } catch(err) {
+        return res.status(400).send({ 
+            status: 400,
+            message: 'failed to register',
+            err: err.message
+        });
+    }
+});
 
 
 module.exports = app => app.use('/user', router);
